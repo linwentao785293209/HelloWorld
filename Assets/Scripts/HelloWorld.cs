c379f5508bbfe0650ddbbdd415656ddaf718251e
linwentao <linwentao@9snail.com>
2023-08-31 11:15:03 +0800

    Gitee To GitHub Hello World

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("My Hello World");
+        Debug.Log("Gitee To GitHub Hello World");
     }
 
     // Update is called once per frame
